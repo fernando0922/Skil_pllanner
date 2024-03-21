69e0e5ddae745570815e4bc2faf2bb93a42085ff
fernando0922 <fernando.vidal.uw@gmail.com>
2024-03-22 05:11:11 +0530

    data in place but no css

--- src/components/TimeTale/TimeTable.jsx
+++ src/components/TimeTale/TimeTable.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -6,31 +6,40 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import rows from "./../../assets/plan.json";
+import { Button } from "@mui/material";
 
 function TimeTable() {
+  const [showTable, setShowTable] = useState(false);
   return (
-    <TableContainer>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>DAYS</TableCell>
-            <TableCell>PRIMARY</TableCell>
-            <TableCell>SECONDARY</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {Object.values(rows).map((row) => (
-            <TableRow key={row.id}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell>{row.primary}</TableCell>
-              <TableCell>{row.secondary}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <Button variant="outlined" onClick={() => setShowTable(!showTable)}>
+        {showTable ? "Hide" : "Show"} Table
+      </Button>
+      {showTable && (
+        <TableContainer>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>DAYS</TableCell>
+                <TableCell>PRIMARY</TableCell>
+                <TableCell>SECONDARY</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {Object.values(rows).map((row) => (
+                <TableRow key={row.id}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell>{row.primary}</TableCell>
+                  <TableCell>{row.secondary}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+    </>
   );
 }
 
